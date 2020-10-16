572c143f65aaa55fed3dcc2a436eb89cc69c8249
Chase Simmons <csim6890@gmail.com>
2020-10-16 16:28:04 -0500

    public sourced and working properly

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,17 +1,5 @@
-// init
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const PORT = process.env.PORT || 5000;
-///
-// app use
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(express.static('public'));
-///
+$(document).ready(readyNow);
 
-// connection listener
-app.listen(PORT, () => {
-  console.log('listening on port', PORT);
-});
-// end of connection listener
+function readyNow() {
+  console.log('init');
+}
