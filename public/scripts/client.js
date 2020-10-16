3367fda1191ca7b4b11fdebfe06b7dd658311143
Chase Simmons <csim6890@gmail.com>
2020-10-16 17:31:55 -0500

    js set up to send to server

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,4 +2,52 @@ $(document).ready(readyNow);
 
 function readyNow() {
   console.log('init');
+  $('.button-box').on('click', '.B', onClick);
+}
+let equationObject = { number1: 0, mathType: '', number2: 0 };
+
+function onClick() {
+  let button = $(this);
+
+  // filter out math type
+  if (button.hasClass('+')) {
+    equationObject.mathType = '+';
+  } else if (button.hasClass('-')) {
+    equationObject.mathType = '-';
+  } else if (button.hasClass('*')) {
+    equationObject.mathType = '*';
+  } else if (button.hasClass('/')) {
+    equationObject.mathType = '/';
+  }
+  ///
+
+  if (button.hasClass('=')) {
+    equationObject.number1 = $('.number1').val();
+    equationObject.number2 = $('.number2').val();
+
+    // send data to server
+    $.ajax({
+      type: 'POST',
+      url: '/calculate',
+      data: equationObject,
+    })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (err) {
+        console.log(err);
+        alert('could not send item data to server');
+      });
+    ///
+  }
+  if (button.hasClass('C')) {
+    clear();
+    equationObject = { number1: 0, mathType: '', number2: 0 };
+    $('.number1').val('');
+    $('.number2').val('');
+  }
+}
+
+function clear() {
+  console.log('cleared');
 }
